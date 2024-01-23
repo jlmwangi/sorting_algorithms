db883de27c8a7a02fc6e9a82b4ed29d1e527bdea
Mwangii <jlmwangi254@gmail.com>
2024-01-23 08:37:18 +0300

    update sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,4 +8,27 @@
 
 void shell_sort(int *array, size_t size)
 {
+	size_t n = 1;
+	size_t i, j;
+	int temp;
 
+	while (n <= size / 3)
+		n = 3 * n + 1;
+
+	while (n > 0)
+	{
+		for (i = n; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= n && array[j - n] > temp)
+			{
+				array[j] = array[j - n];
+				j -= n;
+			}
+			array[j] = temp;
+		}
+		n = (n - 1) / 3;
+	}
+}
